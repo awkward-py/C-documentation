fa19b04110344cc95cf1ef7d6ed2baff227d4328
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-01-06 10:31:43 +0530

    Update First.c

--- 1st Program/First.c
+++ 1st Program/First.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
 
-// The main function where the program starts
-int main() {
-    // Print a warm welcome message
-    printf("Hello, AwkwardPy! You just created your first C program.\n");
-
-    // Return 0 to indicate successful execution
-    return 0;
-}
